d88bb2ae7006bc2ca68b59e3efa3fb401492a641
yangyao <3034789537@qq.com>
2026-05-14 19:17:11 +0800

    feat: 添加 PWA 支持，可安装到手机/电脑桌面离线使用

---
+++ sw.js
@@ -0,0 +1,26 @@
+var CACHE_NAME = "genshin-char-v1";
+var toCache = [
+  "./",
+  "index.html",
+  "css/style.css",
+  "js/data.js",
+  "js/sky.js",
+  "js/main.js",
+  "manifest.json"
+];
+
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(toCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      return r || fetch(e.request);
+    })
+  );
+});
